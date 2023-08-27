977d142fa9e3ab92c21283863da3735f8a4f9d4e
MohammedBelfellah <bmwb6283@gmail.com>
2023-08-27 10:57:53 +0000

    lesonse in js about export && >> import fanctoin from anther file to the main file

---
+++ js/app.js
@@ -0,0 +1,13 @@
+export default function mohammed() {
+    return "this is mohammed belfellah"
+}
+export const belfellah = () => {
+    return "hello brathe this is me her "
+}
+
+
+/* anther way to export  a function */
+const jilali = () => {
+    return 2000 + 4 
+}
+export {jilali}
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import mohammed from "./app.js";
+import {belfellah as bel} from "./app.js"
+import { jilali } from "./app.js";
+
+console.log(mohammed()," + " , bel())
+console.log(jilali())
